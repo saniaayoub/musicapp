11f659dc3d9b8ccb5dd4fb67a75451686837179b
saniaayoub <saniaayoub555@gmail.com>
2022-07-26 20:16:42 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  Text,
+  View,
+  Platform,
+} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {NativeBaseProvider} from 'native-base';
+
+const App = () => {
+   const [userToken, setUserToken] = useState(null);
+   const config = {
+     dependencies: {
+       'linear-gradient': require('react-native-linear-gradient').default,
+     },
+   };
+  return (
+    <NativeBaseProvider config={config}>
+      <SafeAreaProvider>
+        <KeyboardAvoidingView
+          style={{flex: 1}}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <View>
+            <Text>App</Text>
+          </View>
+        </KeyboardAvoidingView>
+      </SafeAreaProvider>
+    </NativeBaseProvider>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
