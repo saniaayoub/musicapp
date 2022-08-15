fd77a972ce0e6b8d9635986aaa0846be3ab254a9
saniaayoub <saniaayoub555@gmail.com>
2022-08-15 14:29:31 +0500

    alpha final

--- src/screens/GetStarted/index.js
+++ src/screens/GetStarted/index.js
@@ -10,30 +10,29 @@ import {
 import React from 'react';
 import s from './style';
 import background from '../../assets/images/background.png';
-import { LinearTextGradient } from 'react-native-text-gradient';
+import {LinearTextGradient} from 'react-native-text-gradient';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button } from 'native-base';
-import { moderateScale } from 'react-native-size-matters';
+import {Input, Button} from 'native-base';
+import {moderateScale} from 'react-native-size-matters';
 import Lock from '../../assets/images/lock.svg';
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
-const GetStarted = ({ navigation }) => {
+const GetStarted = ({navigation}) => {
   return (
-    <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+    <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
       <ImageBackground source={background} blurRadius={5} resizeMode={'cover'}>
-        <View style={[s.container, { width: width, height: height }]}>
-
+        <View style={[s.container, {width: width, height: height}]}>
           <View></View>
           <View>
             <View style={s.heading}>
               <LinearTextGradient
-                style={{ fontWeight: 'bold', fontSize: 72 }}
+                style={{fontWeight: 'bold', fontSize: 72}}
                 locations={[0, 1]}
-                colors={['#C26AF8', '#25ADF9']}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 0, y: 1 }}
+                colors={['rgb(192, 107, 248)', 'rgb(48, 231, 214)']}
+                start={{x: 0, y: 0}}
+                end={{x: 0, y: 1}}
               >
-                <Text style={[s.headingText]} >Energy 4 Living</Text>
+                <Text style={[s.headingText]}>Energy 4 Living</Text>
               </LinearTextGradient>
             </View>
             <View style={s.para}>
@@ -44,7 +43,6 @@ const GetStarted = ({ navigation }) => {
             </View>
           </View>
           <View></View>
-
         </View>
       </ImageBackground>
       <View style={s.button}>
