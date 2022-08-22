f1b3181548e741c06a6109c216067f6ca3fc358e
NoahConner <noahconner1512@gmail.com>
2022-08-22 13:11:40 +0500

    option changes

--- service.js
+++ service.js
@@ -1,12 +1,41 @@
 // service.js
 module.exports = async function() {
 
-    TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
-
-    TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
-
-    TrackPlayer.addEventListener('remote-stop', () => TrackPlayer.destroy());
-
-    // ...
+    TrackPlayer.addEventListener(
+        'remote-play',
+        async () => await TrackPlayer.play(),
+      );
+     
+      TrackPlayer.addEventListener(
+        'remote-pause',
+        async () => await TrackPlayer.pause(),
+      );
+     
+      TrackPlayer.addEventListener(
+        'remote-stop',
+        async () => await TrackPlayer.stop(),
+      );
+     
+      TrackPlayer.addEventListener('remote-jump-backward', async () => {
+        TrackPlayer.seekTo((await TrackPlayer.getPosition()) - 15);
+      });
+     
+      TrackPlayer.addEventListener('remote-jump-forward', async () => {
+        TrackPlayer.seekTo((await TrackPlayer.getPosition()) + 15);
+      });
+     
+      TrackPlayer.addEventListener(
+        'remote-next',
+        async () => await TrackPlayer.skipToNext(),
+      );
+     
+      TrackPlayer.addEventListener(
+        'remote-previous',
+        async () => await TrackPlayer.skipToPrevious(),
+      );
+     
+      TrackPlayer.addEventListener('remote-seek', ({ position }) => {
+        TrackPlayer.seekTo(position);
+      });
 
 };
